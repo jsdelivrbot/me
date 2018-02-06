6b872811b6e39b12d7cdb8e3518871e35f5166fb
Chris Vogt <mail@chrisvogt.me>
2018-02-05 22:47:29 -0800

    Resolve linter warnings.

--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -1,11 +1,10 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { Button, Container, Col, Row } from 'reactstrap';
+import { Container } from 'reactstrap';
 import { connect } from 'react-redux';
 
 import Jumbotron from '../components/Jumbotron';
 import Page from '../components/Page';
-import PageTitle from '../components/PageTitle';
 import ProjectsList from '../components/ProjectsList';
 
 import '../styles/pages/Projects.css';
